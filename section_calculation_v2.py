002a2334cbbae5118f717f852aa9e176b07bfb74
Omar <eng.omarghareeb@gmail.com>
2025-11-23 22:29:34 +0200

    Seprarte file for user input

--- section_calculation_v2.py
+++ section_calculation_v2.py
@@ -9,6 +9,7 @@ class node:
 
     
     def __init__(self,x,y):
+        
         self.id = node.n_id
         node.n_id +=1
         self.id_dict = {}
@@ -19,13 +20,18 @@ class node:
         node.all_nodes[self.id] = self
         
     def __repr__(self):
-        return f"Node: {self.id}, x: {self.x}, y: {self.y}"
+        return f"{self.id},{self.x},{self.y}"
 
 class wall:
+    
+    all_walls = {}
+    
     def __init__(self,node1,node2,t):
         # Nodes
         self.node1 = node1
         self.node2 = node2
+        wall.all_walls[(self.node1,self.node2)] = self
+        
         self.x1 = node1.x
         self.y1 = node1.y
         self.x2 = node2.x
@@ -46,35 +52,27 @@ class wall:
         self.iye = self.iy_axis * np.cos(self.theta)**2
         self.ixye = self.iy_axis/2 * np.sin(2*self.theta)
         
+        section.get_cg()
+        section.get_i()
+        
+    
     def adjacent(self):
         self.node1.id_dict[self.node2.id] = self
         self.node2.id_dict[self.node1.id] = self
+        
+
         
 class boom():
     def __init__(self,boom_node,A):
         self.node = boom_node
         self.A = A
+        section.get_cg()
+        section.get_i()
         
-
-def ask_user():
-    n = int(input("Enter number of nodes "))
-    
-    nodes = [node(0,0) for i in range(n)]
-    for i in range(n):
-        print(f"\nNode {i}: ")
-        nodes[i].x = float(input(f"Enter x{i}: "))
-        nodes[i].y = float(input(f"Enter y{i}: "))
+class section():
+    def get_cg():
+        pass
         
-    nw = int(input("\nEnter number of walls "))
-    walls = []
-    print()
-    
-    for i in range(nw):
-        for j in range(n):
-            print(nodes[j])
-    
-        id_1 = int(input(f"\nChoose first node for wall no.{i+1}: "))
-        id_2 = int(input(f"\nChoose second node for wall no.{i+1}: "))
-        t = float(input(f"\nChoose thickness for wall no.{i+1}: "))
-        walls.append(wall(node.all_nodes[id_1],node.all_nodes[id_2],t))
-        print()
+    def get_i():
+        pass
+        
